796d66d73a57c49e03fe0b53622b4c3acd42ac58
Kamil Smoczynski <kamilsmoczynski9309@gmail.com>
2022-12-30 04:52:30 +0100

    finish burger menu

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const burgerBtn = document.querySelector('.burger')
-const open = document.querySelector('.fa-bars')
-const close = document.querySelector('hide')
+const nav = document.querySelector('ul')
+const openBtn = document.querySelector('.fa-bars')
+const closeBtn = document.querySelector('.fa-times')
 
 
+const handleNav = () => {
+	nav.classList.toggle('active')
+	burgerBtn.classList.toggle('active')
+	openBtn.classList.toggle('hide')
+	closeBtn.classList.toggle('hide')
+}
 
 
-
-
-
-burger
+burgerBtn.addEventListener('click', handleNav)
